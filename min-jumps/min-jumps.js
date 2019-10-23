41a2e242d994483299a4f5fce5a24dc2a97044ed
Alisa Chan <alisachan.ny@gmail.com>
2019-10-23 18:02:37 -0400

    attempted min-jumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const minJumps = arr => {
-
+const minJumps = (arr = [2, 4, 1, 1, 2, 3, 7, 1, 1, 3]) => {
+  let jumpCount = 0;
+  let jumpLength = arr[0];
+  let i = 0;
+  let subArray = arr.slice(i + 1, i + arr[i] + 1);
+  let nextJump = Math.max(...subArray);
+  console.log(arr);
+  while (jumpLength < arr.length) {
+    jumpLength += nextJump;
+    jumpCount++;
+  }
+  return jumpCount;
 };
 
-module.exports = minJumps
+module.exports = minJumps;
